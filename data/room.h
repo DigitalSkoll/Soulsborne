50ac1bc51c40581e888f429143ce57f04cb0738a
HentaiChan <snake_of_yang@yahoo.com>
2016-12-01 19:56:37 -0600

    Add files via upload

---
+++ data/room.h
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class Room ()
+{
+int *NorthDoor;
+int *EastDoor;
+int *SouthDoor;
+int *WestDoor;
+string RoomDesc;
+string ElementDesc;
+string
+string
+string
+};
+
+funct roomRandomizer ()
+{
+
+} 
